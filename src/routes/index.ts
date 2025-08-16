ca8c9ba929b51624d8266fd828b3ddecd774899a
Emtiaz Ahmed <emtiaz.ahmed@g.bracu.ac.bd>
2025-08-16 15:28:00 +0600

    update codes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,17 @@
 // routes/index.ts
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import blogRoutes from "./blogRoutes";
 import contactRoutes from "./contactRoutes";
 import projectRoutes from "./projectRoutes";
 
 const router = Router();
 
+// API root route
+router.get("/", (req: Request, res: Response) => {
+  res.send("🚀 emtiaz's portfolio server is running");
+});
+
+// Sub-routes
 router.use("/projects", projectRoutes);
 router.use("/blogs", blogRoutes);
 router.use("/contact", contactRoutes);
